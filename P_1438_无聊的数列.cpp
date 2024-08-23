3e0ef252d06ccbf58e0bf5b19578a8c537520164
Dark_Modest <darkmodest@hotmail.com>
2024-08-23 21:57:21 +0800

    Update Codes

--- P_1438_无聊的数列.cpp
+++ P_1438_无聊的数列.cpp
@@ -5,11 +5,13 @@ using namespace std;
 const int N = 1e5+5;
 struct segment_tree{
 	int l, r, v;
-	int atag;
+	int dtag, ktag;
 } t[4 * N];
 int n, m;
-int l, r, p;
+int l, r, qp;
 int k, d;
+int a[N];
+int opt;
 int ls(int p){
 	return p << 1;
 }
@@ -21,6 +23,8 @@ void pushup(int p){
 }
 void build(int l, int r, int p){
     if(l == r){
+        t[p].l = l;
+        t[p].r = r;
         t[p].v = a[l];
         return;
     }
@@ -32,10 +36,14 @@ void build(int l, int r, int p){
     pushup(p);
 }
 void addtag(int p, int K, int D){
-    
+    t[p].ktag += K;
+    t[p].dtag += D;
+    t[p].v += (K + (K + D * (t[p].r - t[p].l))) * (t[p].r - t[p].l + 1) / 2;
 }
 void pushdown(int p){
-	int mid = t[p].l + t[p].r >> 1;
+	addtag(ls(p), t[p].ktag, t[p].dtag);
+    addtag(rs(p), t[p].ktag, t[p].dtag);
+    t[p].ktag = t[p].dtag = 0;
 }
 void update(int ql, int qr, int p, int K, int D){
     if(ql <= t[p].l && t[p].r <= qr){
@@ -43,37 +51,39 @@ void update(int ql, int qr, int p, int K, int D){
         return;
     }
     int mid = t[p].l + t[p].r >> 1;
-    if(ql <= mid) update(ql, qr, ls(p));
-    if(mid + 1 <= qr) update(ql, qr, rs(p));
+    //addtag(p, K, D);
+    pushdown(p);
+    if(ql <= mid) update(ql, qr, ls(p), K, D);
+    if(mid + 1 <= qr) update(ql, qr, rs(p), K, D);
     pushup(p);
 }
 int query(int ql, int qr, int p){
     if(ql <= t[p].l && t[p].r <= qr)
         return t[p].v;
     int res = 0;
-    pushdown(p);
     int mid = t[p].l + t[p].r >> 1;
-    if(ql <= mid) res += query(ql, qr, ls(p));
+    pushdown(p);
     if(mid + 1 <= qr) res += query(ql, qr, rs(p));
+    if(ql <= mid) res += query(ql, qr, ls(p));
     return res;
 }
 
 signed main(){
-	//ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-	
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	//freopen("P1438_1.in", "r", stdin);
 	cin >> n >> m;
     for(int i = 1; i <= n; i++)
         cin >> a[i];
 	build(1, n, 1);
+    //cout << "%%%" << endl;
 	for(int i = 1; i <= m; i++){
-		cin >> c;
-		if(c == 1){
-            cin >> l >> r >> K >> D;
-            int ld = K;
+		cin >> opt;
+		if(opt == 1){
+            cin >> l >> r >> k >> d;
 			update(l, r, 1, k, d);
-        } else  {
-            cin >> p;
-			cout << query(p, p, 1) << endl;
+        } else if(opt == 2){
+            cin >> qp;
+			cout << query(qp, qp, 1) << endl;
         }
 	}
 	return 0;
